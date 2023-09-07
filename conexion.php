3a60c1c3ddefd9dfaa5d7db5f6dfdd3e3542d0bd
Gustavo Vasquez <gvasquezdamaso@outlook.com>
2023-09-06 21:21:33 -0600

    Conexion a base de datos con exito

--- conexion.php
+++ conexion.php
@@ -1,14 +1,16 @@
 <?php
 $servername = "localhost";
-$database = "databasename";
-$username = "username";
-$password = "password";
+$database = "chedraui";
+$username = "tavobeto";
+$password = "N1nt3nd0123.,@";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
+    mysqli_close($conn);
 }
-echo "Connected successfully";
-mysqli_close($conn);
+
 ?>
