9ed74f8d3efffd068eaeda657f960173b057dff1
Gustavo Vasquez <gvasquezdamaso@outlook.com>
2023-09-11 08:48:25 -0600

    Se agrego boton de login

--- index.php
+++ index.php
@@ -40,6 +40,11 @@
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
+        <form class="d-flex" role="search" margin>
+            <div class="container">
+            <button class="btn btn-outline-success" type="submit">Iniciar Sesion</button>
+            </div>
+        </form>
         </div>
     </div>
     </nav>
